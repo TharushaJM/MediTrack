42ed1ec07a8413ecf41d988091654693c0d59094
TharushaGonawala <157585076+TharushaJM@users.noreply.github.com>
2025-11-03 20:08:11 +0530

    fixinng database conection

--- Server/src/index.js
+++ Server/src/index.js
@@ -3,3 +3,20 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import userRoutes from  "./routes/userRoutes.js"
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("MediTrack backend running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
